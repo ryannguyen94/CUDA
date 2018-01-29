6fe8ab45fc04bfb7e68a1ba2cd0ed216831ed2a5
ryan <ryan94@uw.edu>
2018-01-28 23:58:44 -0800

    Finish up to prob 5

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -36,7 +36,7 @@ def pfKernel2D (d_x , d_y , d_f):
         d_f[i,j] = pf2D (d_x[i], d_y[j])
 
 def pfArray2D (x, y, TPBX, TPBY):
-
+    
     nx = x.size
     ny = y.size
     d_x = cuda.to_device(x)
@@ -45,42 +45,54 @@ def pfArray2D (x, y, TPBX, TPBY):
     gridDims = ((nx + TPBX - 1) // TPBX,
                 (ny + TPBY - 1) // TPBY)
     blockDims = (TPBX, TPBY)
+
+    startTime = time.time()
     pfKernel2D[gridDims, blockDims](d_x, d_y, d_f)
+    kTime = (time.time() - startTime) * 1000
+    print ("Kernal call time is: ", kTime)
     return d_f.copy_to_host()
 
 def question2():
 
-    TPBX = TPBY = 16
-    NX = [255, 1023, 4095, 16383, 65535]
-    NY = [255, 1023, 4095, 16383, 65535]
+    print ("---------- Question 2 ----------")
 
-    sTime = pTime = accel = [0]*len(NX)
-    # pTime = sTime
-    # accel = sTime
+    TPBX = 8
+    TPBY = 32
+    NX = np.linspace(100, 1000, 10)
+    NY = np.linspace(100, 1000, 10)
+
+    sTime = [0]*len(NX)
+    pTime = [0]*len(NX)
+    accel = [0]*len(NX)
+
     for i in range(len(NX)):
+        print ("Array size: ", NX[i])
         x = np.linspace (0,1,NX[i] , dtype = np.float32)
         y = np.linspace (0,1,NY[i] , dtype = np.float32)
         startTime = time.time()
         fs = fArray2D (x, y)
-        sTime[i] = time.time() - startTime
+        sTime[i] = (time.time() - startTime) * 1000
+        print ("Series processing time: ", sTime[i])
 
         startTime = time.time()
         fp = pfArray2D(x, y, TPBX, TPBY)
-        pTime[i] = time.time() - startTime
+        pTime[i] = (time.time() - startTime) * 1000
+        print ("Parallel processing time: ", pTime[i])
 
-        accel[i] = pTime[i]/sTime[i]
+        accel[i] = sTime[i]/pTime[i]
+        print ("Accel is: ", accel[i])
 
-    plt.figure(1)
-    plt.subplot(211)
-    plt.plot(NX, sTime, 'r--', label='series runtime')
-    plt.plot(NX, pTime, 'g^', label='parallel_runtime')
-    plt.legend()
-    plt.title("Series and Parallel Runtime vs Array Size")
+    # plt.figure(1)
+    # plt.subplot(211)
+    # plt.plot(NX, sTime, 'r--', label='series runtime')
+    # plt.plot(NX, pTime, 'g^', label='parallel_runtime')
+    # plt.legend()
+    # plt.title("Series and Parallel Runtime vs Array Size")
 
-    plt.subplot(212)
-    plt.plot(NX, accel)
-    plt.title("Acceleration vs Array Size")
-    plt.show()
+    # plt.subplot(212)
+    # plt.plot(NX, accel)
+    # plt.title("Acceleration vs Array Size")
+    # plt.show()
 
     # X,Y = np.meshgrid (x, y)
     # plt.contourf (X, Y, f)
@@ -89,15 +101,76 @@ def question2():
 ''' Question 3 functions '''
 def question3():
 
-    TPBX = TPBY = 1000
-    NX = NY = 65535
+    print ("---------- Question 3 ----------")
+
+    TPBX = 32
+    TPBY = 32
+    NX = 255
+    NY = 255
 
     x = np.linspace(0, 1, NX, dtype=np.float32)
     y = np.linspace(0, 1, NY, dtype=np.float32)
 
     fp = pfArray2D(x, y, TPBX, TPBY)
+    print ("32 is the largest number of thread a block can have."
+    	" Anything larger than that will produce the following error:"
+    	" numba.cuda.cudadrv.driver.CudaAPIError: [1] Call to"
+    	" cuLaunchKernel results in CUDA_ERROR_INVALID_VALUE")
+
+''' Question 4 functions '''
+def question4():
+	print ("---------- Question 4 ----------")	
+
+	print ("Change in aspect ratio has no affect on the kernel"
+		" execution time or kernal call")
+
+''' Question 5 functions '''
+def question5(p):
+	print ("---------- Question 5 ----------")
+
+	arrayDimX = 256
+	arrayDimY = 256
+
+	array = [[0]*arrayDimX] * arrayDimY
+	x = np.linspace(0, 2*math.pi, arrayDimX)
+	for i in range(arrayDimY):
+		array[i] = np.sin(np.linspace(x[i], 2*math.pi + x[i], arrayDimX))
+	
+	res = pnorm(array, p)
+	print ("Result is: ", res)
+
+@cuda.jit
+def norm_kernel(d_array, out, p):
+    i , j = cuda.grid (2)
+    nx , ny = d_array.shape
+    if i < nx and j < ny:
+        out[0] += (d_array[i,j] ** p)
+
+
+def pnorm(array, p):
+
+    TPBX = 8
+    TPBY = 8
+
+    nx, ny = np.array(array).shape
+
+    d_array = cuda.to_device(np.array(array))
+    d_out = cuda.to_device(np.zeros(1))
+
+    gridDims = ((nx + TPBX - 1) // TPBX,
+                (ny + TPBY - 1) // TPBY)
+    blockDims = (TPBX, TPBY)
+
+
+    norm_kernel[gridDims, blockDims](d_array, d_out, p)
+    return d_out.copy_to_host()[0] ** (1/p)
 
 def main():
+    # question2()
+    # question3()
+    # question4()
+    question5(100000)
 
-if __name__ == '__main__ ':
-    main ()
+# call to execute main
+if __name__ == '__main__':
+    main()
