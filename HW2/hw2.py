764c702f0e4d72b905fac1ecbe77b59f44a7c72f
ryan <ryan94@uw.edu>
2018-01-29 21:28:09 -0800

    Hw2 finish.Stable

--- HW2/hw2.py
+++ HW2/hw2.py
@@ -10,6 +10,9 @@ import matplotlib.pyplot as plt  # import plotting library
 from numba import cuda
 import math
 import time
+from mpl_toolkits import mplot3d
+
+
 
 ''' Question 2 functions '''
 def f2D (x, y):
@@ -53,13 +56,13 @@ def pfArray2D (x, y, TPBX, TPBY):
     return d_f.copy_to_host()
 
 def question2():
-
+    print ()
     print ("---------- Question 2 ----------")
 
     TPBX = 8
     TPBY = 32
-    NX = np.linspace(100, 3500, 10)
-    NY = np.linspace(100, 3500, 10)
+    NX = np.linspace(100, 1000, 10)
+    NY = np.linspace(100, 1000, 10)
 
     sTime = [0]*len(NX)
     pTime = [0]*len(NX)
@@ -96,7 +99,7 @@ def question2():
 
 ''' Question 3 functions '''
 def question3():
-
+    print ()
     print ("---------- Question 3 ----------")
 
     TPBX = 32
@@ -109,38 +112,90 @@ def question3():
 
     fp = pfArray2D(x, y, TPBX, TPBY)
     print ("32 is the largest number of thread a block can have."
-    	" Anything larger than that will produce the following error:"
-    	" numba.cuda.cudadrv.driver.CudaAPIError: [1] Call to"
-    	" cuLaunchKernel results in CUDA_ERROR_INVALID_VALUE")
+        " Anything larger than that will produce the following error:"
+        " numba.cuda.cudadrv.driver.CudaAPIError: [1] Call to"
+        " cuLaunchKernel results in CUDA_ERROR_INVALID_VALUE")
 
 ''' Question 4 functions '''
 def question4():
-	print ("---------- Question 4 ----------")	
+    print ()
+    print ("---------- Question 4 ----------")  
 
-	print ("Change in aspect ratio has no affect on the kernel"
-		" execution time or kernal call")
+    print ("Change in aspect ratio has very little affect on the kernel"
+        " execution time or kernal call")
 
 ''' Question 5 functions '''
-def question5(p):
-	print ("---------- Question 5 ----------")
+def question5():
+    print ()
+    print ("---------- Question 5 ----------")
 
-	arrayDimX = 256
-	arrayDimY = 256
+    arrayDimX = 255
+    arrayDimY = 255
 
-	array = [[0]*arrayDimX] * arrayDimY
-	x = np.linspace(0, 2*math.pi, arrayDimX)
-	for i in range(arrayDimY):
-		array[i] = np.sin(np.linspace(x[i], 2*math.pi + x[i], arrayDimX))
-	
-	res = pnorm(array, p)
-	print ("Result is: ", res)
+    array = [[0]*arrayDimX] * arrayDimY
+    x = np.linspace(0, 2*math.pi, arrayDimX)
+    y = np.linspace(0, 2*math.pi, arrayDimY)
+    
+    array = make_matrix(x, y)
+
+    X, Y = np.meshgrid(x, y)
+    plt.contourf(X, Y, array)
+    plt.show()
+
+    print ("Compute L2:")
+    res = pnorm(array, 2)
+    print ("Result is: ", res)
+
+    print ("Compute L4:")
+    res = pnorm(array, 4)
+    print ("Result is: ", res)
+
+    print ("Compute L6:")
+    res = pnorm(array, 6)
+    print ("Result is: ", res)
+
+    print ("Compute L1000:")
+    res = pnorm(array, 1000)
+    print ("Result is: ", res)
+
+    print ("The value of norm approaches 1 which is norm infinity as p increases")
+
+
+def make_matrix(x, y):
+    TPBX = 8
+    TPBY = 8
+
+    nx = np.array(x).shape[0]
+    ny = np.array(y).shape[0]
+
+    d_x = cuda.to_device(np.array(x))
+    d_y = cuda.to_device(np.array(y))
+    d_out = cuda.device_array((nx, ny))
+
+    gridDims = ((nx + TPBX - 1) // TPBX,
+                (ny + TPBY - 1) // TPBY)
+    blockDims = (TPBX, TPBY)
+
+
+    make_matrix_kerneld[gridDims, blockDims](d_x, d_y, d_out)
+    return d_out.copy_to_host()
+
 
 @cuda.jit
-def norm_kernel(d_array, out, p):
+def make_matrix_kerneld(d_x, d_y, d_out):
+    i , j = cuda.grid (2)
+    nx = d_x.shape[0]
+    ny = d_y.shape[0]
+    if i < nx and j < ny:
+        d_out[i, j] = math.sin(2*math.pi*d_x[i])*math.sin(2*math.pi*d_y[j])
+
+@cuda.jit
+def norm_kernel(d_array, p):
     i , j = cuda.grid (2)
     nx , ny = d_array.shape
     if i < nx and j < ny:
-        out[0] += (d_array[i,j] ** p)
+        d_array[i,j] = (d_array[i,j] ** p)
+
 
 
 def pnorm(array, p):
@@ -151,70 +206,141 @@ def pnorm(array, p):
     nx, ny = np.array(array).shape
 
     d_array = cuda.to_device(np.array(array))
-    d_out = cuda.to_device(np.zeros(1))
 
     gridDims = ((nx + TPBX - 1) // TPBX,
                 (ny + TPBY - 1) // TPBY)
     blockDims = (TPBX, TPBY)
 
 
-    norm_kernel[gridDims, blockDims](d_array, d_out, p)
-    return d_out.copy_to_host()[0] ** (1/p)
+    norm_kernel[gridDims, blockDims](d_array, p)
+    res = 0
+    d_arrayFlat = d_array.copy_to_host().flatten()
+    for i in range(d_arrayFlat.shape[0]):
+        res += d_arrayFlat[i]
+    return res ** (1/p)
+
 
 ''' Question 6 '''
 def question6():
+    print ()
     print ("---------- Question 6 ----------")
 
-    t = np.linspace(0, 10, 10000)
-    dt = 10/1000
-    x_i = np.linspace(0, 3, 4)
-    v_i = np.linspace(0, 3, 4)
+    print ("For IVPs problems, there is no direct way to parallelize "
+        "the computation over a grid of time intervals because current "
+        "value depends on previous values of each states and thus to get "
+        "value at time k, we need to already compute value of all states "
+        "at time k-1")
 
-    iterate(x_i, v_i, dt)
+    print ("For IVPs problems, there is a way to parallelize over a "
+        "grid of initial conditions because the iteration process for "
+        "each group of initial conditions are independent")
 
-    for i in range(len(t) - 1):
-    	[x[i+1], v[i+1]] = iterate(x[i], v[i], dt)
+    nt = 1000
+    t = np.linspace(0, 10, nt)
+    dt = 1/nt
+    x_i = np.linspace(-3, 3, 50)
+    v_i = np.linspace(-3, 3, 50)
 
-    plt.plot(x, v)
-    plt.show()
+    X,V = np.meshgrid(x_i, v_i)
+
+    for subprob in ["6c", "6d", "6e"]:
+        print ("Subproblem ", subprob)
+        r = iterate(x_i, v_i, dt, nt, subprob)
+
+        fig = plt.figure(6)
+        ax = plt.axes(projection='3d')
+
+
+        ax.scatter3D(X, V, r)
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('r')
+        plt.show()
+
+    
 
 # 6c
-def iterate(x_i, v_i, dt):
+def iterate(x_i, v_i, dt, nt, prob):
     TPBX = 16
-    TPBY = 16
+    TPBV = 16
 
-    nx, nv = np.array((x_i, v_i)).shape
+    nx = np.array(x_i).shape[0]
+    nv = np.array(v_i).shape[0]
+
     d_xi = cuda.to_device(np.array(x_i))
     d_vi = cuda.to_device(np.array(v_i))
-    d_out = cuda.device_array((nx, ny), dtype=np.float32)
+    d_x = cuda.device_array((nx, nv, nt))
+    d_v = cuda.device_array((nx, nv, nt))
+    d_r = cuda.device_array((nx, nv))
 
     gridDims = ((nx + TPBX - 1) // TPBX,
                 (nv + TPBV - 1) // TPBV)
     blockDims = (TPBX, TPBV)
 
-
-    iterate_kernel[gridDims, blockDims](d_xi, d_vi, d_out, dt)
-    return d_out.copy_to_host()
-
-@cuda.jit
-def iterate_kernel(d_xi, d_vi, d_out, dt):
-	i, j = cuda.grid(2)
-    nx , nv = d_out.shape
-
-    if i < nx and j < ny:
-    	for i in range(100):
-
-
+    if prob == "6c":
+        iterate_kernel_6c[gridDims, blockDims](d_xi, d_vi, d_x, d_v, dt, nt, d_r)
+    elif prob == "6d":
+        iterate_kernel_6d[gridDims, blockDims](d_xi, d_vi, d_x, d_v, dt, nt, d_r)
+    elif prob == "6e":
+        iterate_kernel_6e[gridDims, blockDims](d_xi, d_vi, d_x, d_v, dt, nt, d_r)
+    return d_r.copy_to_host()
 
 # 6d
+@cuda.jit
+def iterate_kernel_6d(d_xi, d_vi, d_x, d_v, dt, nt, d_r):
+    i, j = cuda.grid(2)
+    nx = d_xi.shape[0]
+    nv = d_vi.shape[0]
+
+    if i < nx and j < nv:
+        d_x[i, j, 0] = d_xi[i]
+        d_v[i, j, 0] = d_vi[j]
+        for k in range(nt-1):
+            d_v[i, j, k+1] = d_v[i, j, k] + (- d_x[i, j, k] - 0.1 * d_v[i, j, k]) * dt
+            d_x[i, j, k+1] = d_x[i, j, k] + d_v[i, j, k] * dt
+            
+        d_r[i,j] = (d_v[i, j, nt-1] ** 2 + d_x[i, j, nt-1] **2) ** 0.5 /((d_xi[i]**2 + d_vi[j]**2)**0.5)
+
+
+# 6c
+@cuda.jit
+def iterate_kernel_6c(d_xi, d_vi, d_x, d_v, dt, nt, d_r):
+    i, j = cuda.grid(2)
+    nx = d_xi.shape[0]
+    nv = d_vi.shape[0]
+
+    if i < nx and j < nv:
+        d_x[i, j, 0] = d_xi[i]
+        d_v[i, j, 0] = d_vi[j]
+        for k in range(nt-1):
+            d_v[i, j, k+1] = d_v[i, j, k] - d_x[i, j, k] * dt
+            d_x[i, j, k+1] = d_x[i, j, k] + d_v[i, j, k] * dt
+            
+        d_r[i,j] = (d_v[i, j, nt-1] ** 2 + d_x[i, j, nt-1] **2) ** 0.5 /((d_xi[i]**2 + d_vi[j]**2)**0.5)
+
+# 6e
+@cuda.jit
+def iterate_kernel_6e(d_xi, d_vi, d_x, d_v, dt, nt, d_r):
+    i, j = cuda.grid(2)
+    nx = d_xi.shape[0]
+    nv = d_vi.shape[0]
+
+    if i < nx and j < nv:
+        d_x[i, j, 0] = d_xi[i]
+        d_v[i, j, 0] = d_vi[j]
+        for k in range(nt-1):
+            d_v[i, j, k+1] = d_v[i, j, k] + (- d_x[i, j, k] + 0.1*(1-d_x[i, j, k]**2) * d_v[i, j, k]) * dt
+            d_x[i, j, k+1] = d_x[i, j, k] + d_v[i, j, k] * dt
+            
+        d_r[i,j] = (d_v[i, j, nt-1] ** 2 + d_x[i, j, nt-1] **2) ** 0.5 /((d_xi[i]**2 + d_vi[j]**2)**0.5)
 
 
 def main():
-    # question2()
-    # question3()
-    # question4()
-    # question5(10000)
-    question6(1, 1)
+    question2()
+    question3()
+    question4()
+    question5()
+    question6()
 
 # call to execute main
 if __name__ == '__main__':
